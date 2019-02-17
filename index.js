ae8fe2c70b4d5314ce8895ac353065a79efa8874
Milan Dhar <milan.dhar@gmail.com>
2019-02-17 21:45:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber == null) {
-    return `Sorry, we don't have a credit card on file for you..`
+  if (cardNumber === null) {
+    return `Sorry, we don't have a credit card on file for you.`
   }
 }
