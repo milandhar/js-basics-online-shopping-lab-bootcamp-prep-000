28755ae7a271afd39a8637954053b39c324224d0
Milan Dhar <milan.dhar@gmail.com>
2019-02-17 21:39:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
   // write your code here
   for (let i = 0; i < cart.length; i++) {
     if (cart[i].itemName == item) {
-      cart.splice()
+      cart.splice(i, 1)
     }
   }
   
