be484fc99ea899ba871ca9b75633d8c41429f159
Milan Dhar <milan.dhar@gmail.com>
2019-02-17 21:36:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ return counter
 
 function removeFromCart(item) {
   // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].itemName == item) {
+      
+    }
+  }
+  
+  
 }
 
 function placeOrder(cardNumber) {
